424fc020f96d25def9280b3814ed021a88fdc697
mictlantecutli <moisesparra82@hotmail.com>
2024-01-24 15:49:30 -0600

    get transaction

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 const form = document.querySelector(".add");
+const incomeList = document.querySelector("ul.income-list");
+const expenseList = document.querySelector("ul.expense-list"); 
 let transactions = localStorage.getItem("transactions") !== null ? JSON.parse(localStorage.getItem("transactions")) : [];
 
-form.addEventListener("submit", event =>{
-  event.preventDefault();
-  const time = new Date();
+function generateTemplate(id, source, amount, time){
+  return `<li data-id="${id}">
+            <p>
+              <span>${source}</span>
+              <span id="time">${time}</span>
+            </p>
+            $<span>${Math.abs(amount)}</span>
+            <i class="bi bi-trash delete"></i>
+          </li>`;
+          //The Math.abs() METHOD IS TO REMOVE '-' (MINUS) SYMBOL
+}
 
+function addTransactionDOM(id, source, amount, time){
+  if(amount > 0){
+    incomeList.innerHTML += generateTemplate(id, source, amount, time);
+  }else{
+    expenseList.innerHTML += generateTemplate(id, source, amount, time);
+
+  }
+
+}
+function addTransaction(source, amount){
+  const time = new Date();
   const transaction = {
     id: Math.floor(Math.random()*100000),
-    source: form.source.value,
-    amount: form.amount.value,
-    time: `${time.toLocaleTimeString} ${time.toLocaleDateString}`
+    source: source,
+    amount: amount,
+    time: `${time.toLocaleTimeString()} ${time.toLocaleDateString()}`
   };
-
   transactions.push(transaction);
   localStorage.setItem("transactions", JSON.stringify(transactions));
-})
+  addTransactionDOM(transaction.id, source, amount, transaction.time );
+
+}
+form.addEventListener("submit", event =>{
+  event.preventDefault();
+  addTransaction(form.source.value, form.amount.value);
+  //this reset method is when someone fill the form, and after the event it will be clear
+  form.reset();
+});
+
+function getTransaction(){
+  transactions.forEach(transaction => {
+    if(transaction.amount > 0){
+      incomeList.innerHTML += generateTemplate(transaction.id, transaction.source, transaction.amount, transaction.time);
+    }else{
+      expenseList.innerHTML += generateTemplate(transaction.id, transaction.source, transaction.amount, transaction.time);
+    }
+
+  })
+};
+
+getTransaction();
